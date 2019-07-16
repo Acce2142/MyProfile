ae5538c77103edc5cd539ec5e8207d48491af318
Duoqi Zheng <zhedy005@mymail.unisa.edu.au>
2019-07-16 12:06:33 +0930

    Add SQL repo

---
+++ ProjectA.Db/SQLRepository.cs
@@ -0,0 +1,41 @@
+﻿using ProjectA.Core.Interfaces;
+using ProjectA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectA.Db
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public System.Linq.IQueryable<T> Collection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Find(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
