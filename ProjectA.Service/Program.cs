0136881370980ed40ef12c61c6bbb4bda5468986
Duoqi Zheng <zhedy005@mymail.unisa.edu.au>
2019-07-16 11:30:00 +0930

    init commit

---
+++ ProjectA.Service/Program.cs
@@ -0,0 +1,16 @@
+﻿using ProjectA.Core.Services;
+using System;
+
+namespace ProjectA.Service
+{
+    class Program
+    {
+        static void Main()
+        {
+           
+                NetworkHelper helper = new NetworkHelper();
+                helper.GetRequest("www.google.com.au");
+            
+        }
+    }
+}
