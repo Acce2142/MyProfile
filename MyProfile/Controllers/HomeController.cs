aa8044e5cbbf61fa43038d72a3c6235e820cba3a
Duoqi Zheng <zhedy005@mymail.unisa.edu.au>
2019-07-18 20:05:07 +0930

    Add bootstrap to edit and create page

--- MyProfile/Controllers/HomeController.cs
+++ MyProfile/Controllers/HomeController.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using MyProfile.Core.Models;
+using MyShop.Core.Contracts;
 using MyShop.Core.ViewModels;
 
 namespace MyProfile.Controllers
 {
     public class HomeController : Controller
     {
-        
+        IRepository<Profile> context;
+        public HomeController(IRepository<Profile> profileContext)
+        {
+            context = profileContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Profile> profiles = context.Collection().ToList();
+            return View(profiles);
         }
     }
 }
